91400a38c91c135996ff5f038675c4a08250f45b
Thanh Trong <ngthtrong0354@gmail.com>
2024-05-09 17:12:15 +0700

    add complaint UI

--- src/controllers/complaintController.js
+++ src/controllers/complaintController.js
@@ -4,7 +4,10 @@ class ComplaintController {
         res.render('complaints');
     }
 
-
+    getAddComplaint(req, res) {
+        res.render('add_complaint');
+    }
+    
 }
 
 module.exports = new ComplaintController; 
